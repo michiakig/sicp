12fcbcae707826d5956ba2317d028b2d49153add
Michiaki Yamada <spacemanaki@gmail.com>
2010-05-20 00:40:57 -0400

    continuing chapter 1

---
+++ ch1/ex.1.17.scm
@@ -0,0 +1,16 @@
+; exercise 1.17
+; "... suppose we include, together with addition, operations double, which doubles an integer, and halve, which divides an (even) integer by 2. Using these, design a multiplication procedure analogous to fast-expt that uses a logarithmic number of steps."
+
+(define double
+	(lambda (x)
+		(* 2 x)))
+
+(define halve
+	(lambda (x)
+		(/ x 2)))
+
+(define fast-mult
+	(lambda (x y)
+		(cond ((= y 1) x)
+					((even? y) (double (fast-mult x (/ y 2))))
+					(else (+ x (fast-mult x (- y 1)))))))
