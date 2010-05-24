ccbd3fb08dc99e0e4f0ef302424bd0e28dfb8797
Michiaki Yamada <spacemanaki@gmail.com>
2010-05-23 23:13:29 -0400

    comments on ex 1.5

--- ch1/ex-1-5.scm
+++ ch1/ex-1-5.scm
@@ -1,9 +1,21 @@
 ; SICP exercise 1.5
-; Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using applicative-order evaluation or normal-order evaluation. He defines the following two procedures:
+; Ben Bitdiddle has invented a test to determine whether the interpreter he is
+; faced with is using applicative-order evaluation or normal-order evaluation.
+; He defines the following two procedures:
 
 (define (p) (p))
 
 (define (test x y)
-	(if (= x 0)
-			0
-			y))
+  (if (= x 0)
+      0
+      y))
+
+(test 0 (p))
+
+; This will result in an infinite loop in an interpreter which uses applicative-
+; order evaluation (most implementations of Scheme?).  The distinction between
+; normal-order and applicative-order really interests me, because I found the
+; discussion surrounding the derivation of the applicative-order Y combinator 
+; in Friedman and Felleisen's The Little Schemer incredibly fascinating, and I
+; did not fully understand the difference between the two.  I will read on with
+; curiousity... 
