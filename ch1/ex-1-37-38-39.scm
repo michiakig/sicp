881f4ea6fe5e2f3b6a1831db3cd3371c3fb358d4
Michiaki Yamada <spacemanaki@gmail.com>
2010-06-07 00:53:35 -0400

    1.3.3

---
+++ ch1/ex-1-37-38-39.scm
@@ -0,0 +1,25 @@
+; (1.37) basic continued fraction function for
+(define cont-frac
+  (lambda (n d k)
+    (define cont-frac-r
+      (lambda (i)
+        (cond ((= i k) (/ (n i) (d i)))
+              (else (/ (n i) (+ (d i) (cont-frac-r (+ i 1))))))))
+    (cont-frac-r 1)))
+
+; (1.38) approximation of e from Euler's De Fractionibus Continuis 
+; "D sub i are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, ..."
+(define d
+  (lambda (i)
+    (cond ((= i 1) 1)
+          ((= i 2) 2)
+          ((= (remainder (- i 2) 3) 0) (+ (d (- i 1)) (d (- i 2)) (d (- i 3))))
+          (else 1))))
+
+; (1.39) approximation of tangent by J H Lambert, 1770
+(define tan
+  (lambda (x k)
+    (/ x
+       (- 1 (cont-frac (lambda (i) (* x x))
+                       (lambda (i) (+ 1 (* 2 i)))
+                       k)))))
