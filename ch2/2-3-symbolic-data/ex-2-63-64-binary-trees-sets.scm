55055526109bb4443de333eaef48b5cf7ec44d52
Michiaki Yamada <spacemanaki@gmail.com>
2010-07-18 23:36:42 -0400

    sets as binary trees

---
+++ ch2/2-3-symbolic-data/ex-2-63-64-binary-trees-sets.scm
@@ -0,0 +1,72 @@
+; Exercise 2.63, 2.64
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set) 
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+; Exercise 2.64 refers to the following procedures, defined in the text:
+; a. Describe how partial-tree works, and draw the tree produced by (list->tree '(1 3 5 7 9 11)).
+; b. What is the order of growth for list->tree on a list of n elements?
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+; Exercise 2.65. Give a Theta(n) implementation of intersect-set and union-set for the binary tree
+; representations using the results from 2.64 and 2.63.
+(define (bin-intersection-set s1 s2)
+  (list->tree (intersection-set (tree->list-1 s1) (tree->list-1 s2))))
+(define (bin-union-set s1 s2)
+  (list->tree (union-set (tree->list-1 s1) (tree->list-1 s2))))
