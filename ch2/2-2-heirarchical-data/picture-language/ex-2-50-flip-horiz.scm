3b5cdf1e745ed2ea7c6ee74d730425cdfb53fb01
aki <spacemanaki@gmail.com>
2010-08-08 00:01:19 -0400

    fixed bug in picture language flip-horiz

--- ch2/2-2-heirarchical-data/picture-language/ex-2-50-flip-horiz.scm
+++ ch2/2-2-heirarchical-data/picture-language/ex-2-50-flip-horiz.scm
@@ -1,12 +1,12 @@
 #lang scheme
 (require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
 
-(define (flip-horiz_ painter)
-  (lambda (frame)
-    (transform-painter_ painter (make-vect 0 0) (make-vect 1 0) (make-vect 0 1))))
-
+(define (my-flip-horiz painter)
+    (my-transform-painter painter (make-vect 1.0 0.0)
+				(make-vect 0.0 0.0)
+                                (make-vect 1.0 1.0)))
 ; defined for us: 
-(define (transform-painter_ painter origin corner1 corner2)
+(define (my-transform-painter painter origin corner1 corner2)
   (lambda (frame)
     (let ((m (frame-coord-map frame)))
       (let ((new-origin (m origin)))
