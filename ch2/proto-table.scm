0972b19a5d45e5ccaedbd49eb1d805a7fecf4eef
aki <spacemanaki@gmail.com>
2010-08-22 15:02:06 -0400

    prototype table using alists for 2.4 and 2.5 exercises

---
+++ ch2/proto-table.scm
@@ -0,0 +1,29 @@
+; In section 2.4.3 the authors introduce "data-directed programming," and describe a
+; table of operations for the complex-number system. But they put off implementing
+; the table itself until section 3.3.3, which means it's hard to try out any of the
+; exercises yet. So here is a simple alist-based table to tide me over until then.
+
+(define table '())
+
+(define (lookup key alist)
+  (cond ((null? alist) #f)
+        ((equal? key (caar alist)) (cdar alist))
+        (else (lookup key (cdr alist)))))
+
+(define (install key value alist)
+  (cond ((null? alist) (cons (cons key value) '()))
+        ((equal? key (caar alist))
+         (cons (cons key value) (cdr alist)))
+        (else (cons (car alist)
+                    (install key value (cdr alist))))))
+
+(define (get op type)
+  (let ((row (lookup op table)))
+    (if row
+        (lookup type row)
+        #f)))
+
+(define (put op type item)
+  (let ((row (lookup op table)))
+    (let ((new-row (install type item (if row row '()))))
+      (set! table (install op new-row table)))))
