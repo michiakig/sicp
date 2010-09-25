65d2fd5c727b99c4cae48cf5564e2277de4ca958
aki <spacemanaki@gmail.com>
2010-09-24 20:42:44 -0400

    link to pg's 'revenge of the nerds'

--- ch3/ex-3-01-acc-gen.scm
+++ ch3/ex-3-01-acc-gen.scm
@@ -1,6 +1,7 @@
 ;; Exercise 3.1
 
-;; Obligatory... http://www.paulgraham.com/icad.html 
+;; obligatory link to pg's "revenge of the nerds"
+;; http://www.paulgraham.com/icad.html 
 (define (make-accumulator n)
   (lambda (i)
     (set! n (+ n i))
--- ch3/ex-3-21-22-23-queues.scm
+++ ch3/ex-3-21-22-23-queues.scm
@@ -9,6 +9,8 @@
   'ok)
 
 ;; Exercise 3.22
+
+; Implements a queue in terms of a procedure with local state.
 (define (make-queue-proc)
   (let ((front-ptr '())
 	(rear-ptr '()))
@@ -41,3 +43,70 @@
 	    ((eq? m 'insert) insert)
 	    ((eq? m 'delete) delete)))
     dispatch))
+
+; external facing procedures
+(define (insert q item) ((q 'insert) item))
+(define (delete q) ((q 'delete)))
+(define (front q) ((q 'front)))
+
+;; Exercise 3.23
+
+; Implementation of a double ended queue
+
+; dequeue is a list - either the empty list when empty or a list containing three
+; elements: if it has one element, the first and third point to the same element
+; and the second is set to '(). if it has more than one, the first points to the 
+; front of the dequeue, the second to the second to last element, and the third to
+; the rear (last element)
+
+(define (make-dequeue) (list))
+(define (empty-dequeue? q) (null? q))
+
+(define (front-deqeue q)
+  (if (empty-dequeue? q)
+      (error "FRONT called on empty dequeue")
+      (caar q)))
+
+(define (rear-dequeue q)
+  (if (empty-dequeue q)
+      (error "REAR called on empty dequeue")
+      (third q)))
+  
+(define (front-insert-dequeue! q item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-dequeue? q)
+	   (list new-pair '() new-pair))
+	  (else
+	     (if (null? (second q))
+		 (set-cdr! new-pair (car q))
+		 (set-car! q new-pair)	; set the first to new-pair
+		 (set-cad)))
+	   (set-cdr! new-pair (first q))
+	   (set-car! q new-pair)
+	   q))))
+
+
+(define (front-delete-dequeue! q)
+  (cond ((empty-dequeue? q)
+	 (error "DELETE! called on empty queue"))
+	(else
+	 (set-car! q (cdar q))
+	 q)))
+
+(define (rear-insert-dequeue! q item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-dequeue? q)
+	   (set-car! q new-pair)
+	   (set-cdr! q (car q))
+	   q)
+	  (else
+	   (let ((end (cdr q)))
+	     (set-cdr! end new-pair)
+	     (set-cdr! q new-pair)
+	     q)))))
+
+(define (rear-delete-dequeue! q)
+  (cond ((empty-dequeue? q)
+	 (error "DELETE! called on an empty queue"))
+	(else
+	 ())))
