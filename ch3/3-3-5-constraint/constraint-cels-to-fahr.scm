ba9f9595ad355ffdf6eb079c0537cccb65da24d8
aki <spacemanaki@gmail.com>
2010-10-11 22:54:55 -0400

    constraint implementation from the text

---
+++ ch3/3-3-5-constraint/constraint-cels-to-fahr.scm
@@ -0,0 +1,16 @@
+;;;; Celsius to Fahrenheit converter using the constraint system in 3.3.5
+
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
+        (v (make-connector))
+        (w (make-connector))
+        (x (make-connector))
+        (y (make-connector)))
+    (multiplier c w u)
+    (multiplier v x u)
+    (adder v y f)
+    (constant 9 w)
+    (constant 5 x)
+    (constant 32 y)
+    'ok))
+
