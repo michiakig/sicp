ca4cfa10963d80d57eb90c9b7d357e2fe56c3965
Michiaki Yamada <spacemanaki@gmail.com>
2010-11-01 22:45:41 -0400

    added prereq to ex 3.56 to utils file

--- ch3/3.5-streams/stream-utils.lisp
+++ ch3/3.5-streams/stream-utils.lisp
@@ -43,3 +43,20 @@
 
 (defun add-streams (s1 s2)
   (stream-map #'+ s1 s2))
+
+;; merge procedure from exercise 3.56
+
+(defun merge (s1 s2)
+  (cond ((stream-null? s1) s2)
+	((stream-null? s2) s1)
+	(t
+	 (let ((s1car (stream-car s1))
+	       (s2car (stream-car s2)))
+	   (cond ((< s1car s2car)
+		  (cons-stream s1car (merge (stream-cdr s1) s2)))
+		 ((> s1car s2car)
+		  (cons-stream s2car (merge s1 (stream-cdr s2))))
+		 (t
+		  (cons-stream s1car
+			       (merge (stream-cdr s1)
+				      (stream-cdr s2)))))))))
