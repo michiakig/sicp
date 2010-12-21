7c4ec190aebfb3fae71c87418e4605ec93f1cfaa
Michiaki Yamada <spacemanaki@gmail.com>
2010-12-21 14:35:48 -0500

    exercies 4.25, 4.27

---
+++ ch4/ex-4.25-4.27.scm
@@ -0,0 +1,52 @@
+;;;; Exercise 4.25
+
+;; from the text:
+
+(define (unless condition usual-value exceptional-value)
+  (if condition exceptional-value usual-value))
+
+(define (factorial n)
+  (unless (= n 1)
+          (* n (factorial (- n 1)))
+          1))
+
+;; If this procedure were evaluated in an applicative-order Scheme,
+;; it would enter an infinite loop, since each call to `unless` would
+;; evaluate its arguments, including a recursive call to `factorial`.
+
+;; In a normal-order Scheme, this would work.
+
+;;;; Exercise 4.27
+
+(define count 0)
+
+(define (id x)
+  (set! count (+ count 1))
+  x)
+
+(define w (id (id 10)))
+
+count
+;; 0
+w
+;; 10
+count
+;; 2
+
+;; I was actually wrong above, my intuition was that in the definition
+;; (define w (id (id 10))), (id (id 10)) would be delayed, and so count
+;; would be 0. But the definition is evaluated, which results in the
+;; definition value is evaluated, and this is an application, so the
+;; inner call to id is delayed but the outer is not.
+
+(define (square x)
+  (* x x))
+
+(square (id 10))
+
+;; 100
+
+count
+
+;; When the evaluator uses memoization, count will be 1, and when it
+;; doesn't it will be 2.
