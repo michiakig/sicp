13cf87e6d3a5a71f1e316f8a252c1bdd00d270c2
Michiaki Yamada <spacemanaki@gmail.com>
2011-01-17 23:48:56 -0500

    ex 5.9

---
+++ ch5/ex-5.09.scm
@@ -0,0 +1,21 @@
+;;;; Structure and Interpretation of Computer Programs
+;;;; Chapter 5 Section 2 A Register-Machine Simulator
+
+;;;; Exercise 5.09
+
+;; I don't know if this is correct - it's not entirely clear what
+;; "enforce the condition that operations can be used only with
+;; registers and constants" but this new version of
+;; make-operation-expression signals an error if one of the operands
+;; of an operation is not a constant or register value
+
+(define (make-operation-exp exp machine labels operations)
+  (let ((op (lookup-prim (operation-exp-op exp) operations))
+        (aprocs
+         (map (lambda (e)
+                (if (not (or (constant-exp? e) (register-exp? e)))
+                    (error "MAKE-OPERATION-EXP -- OPERAND NEITHER CONST NOR REG" e)
+                    (make-primitive-exp e machine labels)))
+              (operation-exp-operands exp))))
+    (lambda ()
+      (apply op (map (lambda (p) (p)) aprocs)))))
