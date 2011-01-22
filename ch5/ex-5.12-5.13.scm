00ccaf9230d788baac752e8d067960cbf2d3e4c3
Michiaki Yamada <spacemanaki@gmail.com>
2011-01-22 15:16:45 -0500

    exercises 5.12, 5.13

--- ch5/ex-5.12-5.13.scm
+++ ch5/ex-5.12-5.13.scm
@@ -14,6 +14,21 @@
 (define (instruction-text-op inst) (car (instruction-text inst)))
 (define (instruction-text-reg inst) (cadadr (instruction-text inst)))
 
+(define (assign-reg-target inst) (cadr (instruction-text inst)))
+(define (assign-reg-src inst) (caddr (instruction-text inst)))
+
+;; used in ex 13 to allocated registers during assembly
+(define (get-registers-referenced inst)
+  (let ((rest (map cadr (filter (lambda (arg)
+                                (eq? (car arg) 'reg))
+                                (cddr (instruction-text inst))))))
+    (if (or (eq? (instruction-text-op inst) 'assign)
+            (eq? (instruction-text-op inst) 'save)
+            (eq? (instruction-text-op inst) 'restore))
+        (cons (assign-reg-target inst)
+              rest)
+        rest)))
+
 (define (update-insts! insts labels machine)
   (let ((pc (get-register machine 'pc))
         (flag (get-register machine 'flag))
@@ -23,11 +38,14 @@
         (list-insts '())
         (list-entries '())
         (list-saved '())
-        (list-sources '()))
+        (list-sources '())
+
+        (registers-seen '()))
     
     (for-each
      (lambda (inst)
-       
+
+       ;; for ex 12 
        (if (not (member (instruction-text inst) list-insts))
            (set! list-insts (cons (instruction-text-op inst) list-insts)))
 
@@ -41,16 +59,24 @@
            (set! list-saved (cons (cadr (instruction-text inst))
                                   list-saved)))
        
-       (if (eq? (instruction-text-op inst)
-                'assign)
-           (let ((record (assoc (cadr (instruction-text inst)) list-sources)))
+       (if (eq? (instruction-text-op inst) 'assign)
+           (let ((record (assoc (assign-reg-target inst) list-sources)))
              (if record
-                 (set-cdr! record (cons (cddr (instruction-text inst))
-                                        (cdr record)))
+                 (set-cdr! record (cons (assign-reg-src inst) (cdr record)))
                  (set! list-sources
-                       (cons (cons (cadr (instruction-text inst))
-                                   (list (cddr (instruction-text inst))))
+                       (cons (cons (assign-reg-target inst)
+                                   (list (assign-reg-src inst)))
                              list-sources)))))
+
+       ;; ex 12, if the inst contains a register we haven't seen yet,
+       ;; allocate it
+       (for-each
+        (lambda (reg)
+          ((machine 'allocate-register) reg)
+          (set! registers-seen (cons reg registers-seen)))
+        (remove-duplicates (filter (lambda (reg)
+                                     (not (member reg registers-seen)))
+                                   (get-registers-referenced inst))))
        
        (set-instruction-execution-proc! 
         inst
@@ -67,9 +93,10 @@
 
 (define (make-machine register-names ops controller-text)
   (let ((machine (make-new-machine)))
-    (for-each (lambda (register-name)
-                ((machine 'allocate-register) register-name))
-              register-names)
+    ;; for ex 13, we allocate registers during assemble instead
+    ;; (for-each (lambda (register-name)
+    ;;             ((machine 'allocate-register) register-name))
+    ;;           register-names)
     ((machine 'install-operations) ops)
     (let ((assembled (assemble controller-text machine)))
       ((machine 'install-instruction-sequence) (car assembled))
@@ -103,14 +130,12 @@
                   (cons (list name (make-register name))
                         register-table)))
         'register-allocated)
-      
+
       (define (lookup-register name)
         (let ((val (assoc name register-table)))
-          (if (not val)
-              (begin
-                (allocate-register name)
-                (cadr (assoc name register-table)))
-              (cadr val))))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
       
       (define (execute)
         (let ((insts (get-contents pc)))
@@ -124,12 +149,7 @@
         (set! sorted-insts (first lst))
         (set! entry-points (second lst))
         (set! saved-or-restored (third lst))
-        (set! sources (list-ref lst 3))
-        
-        ;; (for-each (lambda (register-name)
-        ;;             (allocate-register register-name))
-        ;;           (map car sources))
-        )
+        (set! sources (list-ref lst 3)))
       
       (define (dispatch message)
         (cond ((eq? message 'start)
@@ -165,13 +185,10 @@
   (make-machine
    '()
    (list (list '* *) (list '- -) (list '= =))
-   '((assign continue (label fact-done))     ; set up final return address
+   '((assign continue (label fact-done))
     fact-loop
     (test (op =) (reg n) (const 1))
     (branch (label base-case))
-    ;; Set up for the recursive call by saving n and continue.
-    ;; Set up continue so that the computation will continue
-    ;; at after-fact when the subroutine returns.
     (save continue)
     (save n)
     (assign n (op -) (reg n) (const 1))
@@ -180,9 +197,15 @@
     after-fact
     (restore n)
     (restore continue)
-    (assign val (op *) (reg n) (reg val))   ; val now contains n(n - 1)!
-    (goto (reg continue))                   ; return to caller
+    (assign val (op *) (reg n) (reg val))
+    (goto (reg continue))
     base-case
-    (assign val (const 1))                  ; base case: 1! = 1
-    (goto (reg continue))                   ; return to caller
+    (assign val (const 1))
+    (goto (reg continue))
     fact-done)))
+
+(begin
+  (set-register-contents! recur-fact-machine 'n 5)
+  (start recur-fact-machine)
+  (get-register-contents recur-fact-machine 'val))
+
